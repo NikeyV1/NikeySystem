9af3d00f9f11c87c1c8edd4cb96efa9645e90a09
Nikey <130069847+NikeyV1@users.noreply.github.com>
2024-11-11 15:42:13 +0100

    Update WorldAPI.java

--- src/main/java/de/nikey/nikeysystem/Server/API/WorldAPI.java
+++ src/main/java/de/nikey/nikeysystem/Server/API/WorldAPI.java
@@ -15,5 +15,24 @@ public class WorldAPI {
         return NikeySystem.getPlugin().getConfig().getStringList("system.world.autostartworlds");
     }
 
-    p
+    public static void addAutoStartToWorld(World world) {
+        List<String> worlds = NikeySystem.getPlugin().getConfig().getStringList("system.world.autostartworlds");
+
+        if (!worlds.contains(world)) {
+            worlds.add(world);
+            NikeySystem.getPlugin().getConfig().set("system.world.autostartworlds", worlds);
+            NikeySystem.getPlugin().saveConfig();
+        }
+    }
+
+    public static void unblockCommandForPlayer(Player player, String command) {
+        String playerName = player.getName();
+        List<String> blockedCommands = NikeySystem.getPlugin().getConfig().getStringList("blockedCommands." + playerName);
+
+        if (blockedCommands.contains(command)) {
+            blockedCommands.remove(command);
+            NikeySystem.getPlugin().getConfig().set("blockedCommands." + playerName, blockedCommands);
+            NikeySystem.getPlugin().saveConfig();
+        }
+    }
 }
